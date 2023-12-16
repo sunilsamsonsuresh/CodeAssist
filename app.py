de6e48b7c382990c890b52aa1ce3a0c832812991
sunilsamsonsuresh <sunilsamson.learning@gmail.com>
2023-12-16 16:24:45 +0530

    load model and infer from CPU
    Loading DeciLM from CPU - requires 50+ GB CPU

--- app.py
+++ app.py
@@ -3,30 +3,54 @@ from huggingface_hub import hf_hub_download
 from langchain.prompts import PromptTemplate
 from langchain.llms import CTransformers
 from ctransformers import AutoModelForCausalLM
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
+
+
+model_name = "Deci/DeciLM-7B-instruct"
+
+device = "cuda" # for GPU usage or "cpu" for CPU usage
+
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit_fp32_cpu_offload = True,
+    #load_in_8bit = True,
+    bnb_8bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="auto",
+    trust_remote_code=True,
+    quantization_config=bnb_config
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token = tokenizer.eos_token
 
 
 def complete_code(coding_lang, instruction):
-    llm = CTransformers(model="<model_path>",
-                      model_type='llama',
-                      config={'max_new_tokens':512,
-                              'temperature':0.01,
-                              'gpu_layers': 50})
+    llm = pipeline("text-generation",
+                          model=model,
+                          tokenizer=tokenizer,
+                          temperature=0.1,
+                          device_map="auto",
+                          max_length=4096,
+                          return_full_text=False)
 
-    template = """
-    You are an exceptionally intelligent {coding_lang} coding assistant that \n
+    template ="""
+    ### System:
+    You are an exceptionally intelligent programmer who \n
     consistently delivers accurate and reliable responses to user instructions.
-
-    You are supposed to output only the code based on the instruction given below.
-
-    {instruction}
-
+    ### User:
+    {instruction} in {coding_lang}
+    ### Assistant:
     """
 
     prompt = PromptTemplate(input_variables=["coding_lang", "instruction"],
                             template=template)
 
     response = llm(prompt.format(coding_lang=coding_lang, instruction=instruction))
-
+    response = response[0]['generated_text']
     return response
 
 
@@ -47,7 +71,7 @@ submit = st.button("Generate")
 
 if submit:
     output_text = complete_code(coding_lang, instruction)
-    code_start_index = output_text.find("*/")
+    code_start_index = output_text.find("```")
 
     text_before_code = output_text[:code_start_index]
     code_portion = output_text[code_start_index + 2:]
